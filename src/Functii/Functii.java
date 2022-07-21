b8ee6ea1d7dd52026dae509a96868140d6c8f470
SvSilviu <silviu.stan18@gmail.com>
2022-07-22 01:07:01 +0300

    al doilea commit

--- src/Functii/Functii.java
+++ src/Functii/Functii.java
@@ -1,4 +1,5 @@
 package Functii;
+
 public class Functii {
 
     public static int algoritmInversare(int numar) {
@@ -11,6 +12,7 @@ public class Functii {
         }
         return i;
     }
+
     public static int cifraMax(int numar) {
         int max = -1;
         while (numar != 0) {
@@ -22,6 +24,7 @@ public class Functii {
         }
         return max;
     }
+
     public static int cifraMin(int numar) {
         int min = 10;
         while (numar != 0) {
@@ -33,6 +36,7 @@ public class Functii {
         }
         return min;
     }
+
     public static int cifraParaMax(int numar) {
         int c = 0;
         while (numar != 0) {
@@ -44,6 +48,7 @@ public class Functii {
         }
         return c;
     }
+
     public static int cifraParaMin(int numar) {
         int c = 10;
         while (numar != 0) {
@@ -55,6 +60,7 @@ public class Functii {
         }
         return c;
     }
+
     public static int cifraImparaMax(int numar) {
         int c = -1;
         while (numar != 0) {
@@ -66,6 +72,7 @@ public class Functii {
         }
         return c;
     }
+
     public static int cifraImparaMin(int numar) {
         int c = 10;
         while (numar != 0) {
@@ -77,6 +84,7 @@ public class Functii {
         }
         return c;
     }
+
     public static int contorCifre(int numar) {
         int contor = 0;
         while (numar != 0) {
@@ -86,6 +94,7 @@ public class Functii {
         }
         return contor;
     }
+
     public static int contorCifrePare(int numar) {
         int contor = 0;
         while (numar != 0) {
@@ -98,95 +107,91 @@ public class Functii {
         }
         return contor;
     }
+
     public static int contorCifreImpare(int numar) {
         return contorCifre(numar) - contorCifrePare(numar);
     }
 
+    public static int sumaCifrePare(int numar) {
+        int suma = 0;
+        while (numar != 0) {
+            int cifre = numar % 10;
+            if (cifre % 2 == 0) {
+                suma = suma + cifre;
+            }
+            numar = numar / 10;
+        }
+        return suma;
     }
 
+    public static int sumaCifreImpare(int numar) {
+        int suma = 0;
+        while (numar != 0) {
+            int cifraImpara = numar % 10;
+            if (cifraImpara % 2 != 0) {
+                suma = suma + cifraImpara;
+            }
+            numar = numar / 10;
+        }
+        return suma;
+    }
+
+    public static int numereIntregiSiOperator(int a, int b, char c) {
+        switch (c) {
+            case '+':
+                return a + b;
+            case 's':
+                return a - b;
+            case 'p':
+                return a * b;
+            case 'i':
+                return a / b;
+            case '%':
+                return a % b;
+            default:
+                return -1;
+        }
+    }
+
+    public static int sumaCifre(int numar){
+        return sumaCifreImpare(numar)+sumaCifrePare(numar);
+    }
+    public static int produsCifre(int numar){
+        return sumaCifrePare(numar)*sumaCifreImpare(numar);
+    }
+    public static int scadereCifre(int numar){
+        return sumaCifrePare(numar)-sumaCifreImpare(numar);
+    }
+    public static int impartireCifre(int numar){
+        return sumaCifreImpare(numar)/sumaCifrePare(numar);
+    }
+    public static void Solutie(int numar){
+        String mesaj = "Numarul este : "+numar + "\n";
+        mesaj+="Inversarea numarului este : " + algoritmInversare(numar) + "\n";
+        mesaj+="Cifra maxima a numarului este : " +cifraMax(numar)+ "\n";
+        mesaj+="Cifra minima a numarului este : " +cifraMin(numar)+ "\n";
+        mesaj +="Cifra para maxima a numarului este : " +cifraParaMax(numar)+ "\n";
+        mesaj+="Cifra para minima a numarului este : " +cifraParaMin(numar)+ "\n";
+        mesaj+="Cifra impara maxima a numarului este : "+cifraImparaMax(numar)+"\n";
+        mesaj+="Cifra impara minica a numarului este : "+cifraImparaMin(numar)+"\n";
+        mesaj+="Numarul total de cifre ale numarului este : "+contorCifre(numar)+"\n";
+        mesaj+="Numarul total de cifre pare ale numarului este : "+contorCifrePare(numar)+"\n";
+        mesaj+="Numarul total de cifre impare ale numarului este : "+contorCifreImpare(numar)+"\n";
+        mesaj+="Suma cifrelor pare din numar este : "+sumaCifrePare(numar)+"\n";
+        mesaj+="Suma cifrelor impare din numar este : "+sumaCifreImpare(numar)+"\n";
+        mesaj+="Suma cifrelor numarului este : "+sumaCifre(numar)+"\n";
+        mesaj+="Produsul cifrelor numarului este : "+produsCifre(numar)+"\n";
+        mesaj+="Scaderea cifrelor numarului este : "+scadereCifre(numar)+"\n";
+        mesaj+="Impartirea cifrelor numarului este : "+impartireCifre(numar)+"\n";
+        mesaj+="Numere intregi si operator : " +numereIntregiSiOperator(5,3,'a')+"\n";
+        System.out.println(mesaj);
+
+
+    }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-//
-//    public static int maxim(int a, int b) {
-//
-//        if (a > b) {
-//            return a;
-//        } else {
-//
-//            return b;
-//        }
-//    }
-//
-//    //todo: functie ce primeste ca parametru 2 numere intregi si un operator("
-//    //si returneaza rezultatul
-//
-//    public static int numereIntregi(int x, int z, char i) {
-//
-//        switch (i) {
-//            case '+':
-//                return x + z;
-//            case '-':
-//                return x - z;
-//            case '*':
-//                return x * z;
-//            case '/':
-//                return x / z;
-//            case '%':
-//                return x % z;
-//            default:
-//                return -1;
-//
-//        }
-//
-//    }
-//
-//
-//    //todo:functie ce  primeste ca parametru un nmar si returneaza numarul de cifre
-//
-//
-//    public static int contorCifre(int a) {
-//
-//        int contor = 0;
-//
-//        while (a != 0) {
-//            contor++;
-//            a = a / 10;
-//        }
-//
-//        return contor;
-//    }
-//
-//
-//    //todo:functie ce returneaza numarul de cifre pare
-//    //234234
-//    public static int contorCifrePare(int a) {
-//
-//        int contor = 0;
-//        while (a != 0) {
-//            int cifra = a % 10;
-//            if (cifra % 2 == 0) {
-//                contor++;
-//
-//            }
-//            a = a / 10;
-//        }
-//
-//        return contor;
-//    }
-//
-//    public static int contorCifreImpare(int numar) {
-//        return contorCifre(numar) - contorCifrePare(numar);
-//    }
 //
 //
 //    public static void solutie(int numar) {
@@ -213,18 +218,6 @@ public class Functii {
 //        return a;
 //    }
 //
-//    public static int cifraMin(int numar) {
-//
-//        int a = 10;
-//        while (numar != 0) {
-//            int cifra = numar % 10;
-//            if (cifra <= a) {
-//                a = cifra;
-//            }
-//            numar = numar / 10;
-//        }
-//        return a;
-//    }
 //
 //    public static void solutie2(int numar2) {
 //        String text = "Numarul este : " + numar2 + "\n";
--- src/Functii/Main.java
+++ src/Functii/Main.java
@@ -1,6 +1,5 @@
 package Functii;
 
-
 import static Functii.Functii.*;
 
 public class Main {
@@ -25,16 +24,24 @@ public class Main {
 //
 //        solutie3(345555779);
 
-        System.out.println(algoritmInversare(12345));
-        System.out.println(cifraMax(258));
-        System.out.println(cifraMin(587));
-        System.out.println(cifraParaMax(567));
-        System.out.println(cifraImparaMax(1234));
-        System.out.println(contorCifre(343));
-        System.out.println(contorCifrePare(48186));
-        System.out.println(contorCifreImpare(362011));
-        System.out.println(cifraParaMin(286));
-        System.out.println(cifraParaMax(345));
-        System.out.println(cifraImparaMin(39));
+//        System.out.println(algoritmInversare(12345));
+//        System.out.println(cifraMax(258));
+//        System.out.println(cifraMin(587));
+//        System.out.println(cifraParaMax(567));
+//        System.out.println(cifraImparaMax(1234));
+//        System.out.println(contorCifre(343));
+//        System.out.println(contorCifrePare(48186));
+//        System.out.println(contorCifreImpare(362011));
+//        System.out.println(cifraParaMin(286));
+//        System.out.println(cifraParaMax(345));
+//        System.out.println(cifraImparaMin(39));
+//        System.out.println(sumaCifrePare(4568));
+//        System.out.println(sumaCifreImpare(3689));
+//       System.out.println(sumaCifreImpare(1325));
+//       System.out.println(numereIntregiSiOperator(236,51,'s'));
+//        System.out.println(sumaCifre(32));
+//        System.out.println(produsCifre(38));
+        Solutie(1452);
+        
     }
 }
